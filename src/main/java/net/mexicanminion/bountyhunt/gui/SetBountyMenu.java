1b0930a7807f4016602f06474f69fb1110428ee5
Javier <moncadajavier2@yahoo.com>
2025-05-20 00:08:28 -0500

    start of SetBountyMenu

---
+++ src/main/java/net/mexicanminion/bountyhunt/gui/SetBountyMenu.java
@@ -0,0 +1,25 @@
+package net.mexicanminion.bountyhunt.gui;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+public class SetBountyMenu extends AbstractContainerMenu {
+
+    protected SetBountyMenu(@Nullable MenuType<?> pMenuType, int pContainerId) {
+        super(pMenuType, pContainerId);
+    }
+
+    @Override
+    public ItemStack quickMoveStack(Player player, int i){
+        return null;
+    }
+
+    @Override
+    public boolean stillValid(Player pPlayer) {
+        return false;
+    }
+
+}
